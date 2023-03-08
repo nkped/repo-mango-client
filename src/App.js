f4ada733ad9388dbb43ba4efde42cfded31ec0eb
Nils-Kristian Pedersen <nikrpedev@gmail.com>
2023-03-08 16:29:30 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import {useNavigate} from 'react-router-dom'
+
 
 function App() {
+  const navigate = useNavigate()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:'90%', margin: 'auto auto', textAlign: 'center'}}>
+   <h1>Velkommen fra preees layer!</h1>
+   <Button onClick={() => navigate('create')} style={{width: '100%'}}
+  variant='outline-dark' >Next</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import App from './App';
+import CreatePost from './components/CreatePost.jsx';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/create" element={<CreatePost />} />
+
+    
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
